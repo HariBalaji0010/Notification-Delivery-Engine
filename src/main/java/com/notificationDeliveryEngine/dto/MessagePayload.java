a7ee82c4d78a0f1f00d7252751eb53c597011510
Hari Balaji R <113185180+HariBalaji0010@users.noreply.github.com>
2026-02-16 11:53:51 +0530

    Update MessagePayload.java

--- src/main/java/com/notificationDeliveryEngine/dto/MessagePayload.java
+++ src/main/java/com/notificationDeliveryEngine/dto/MessagePayload.java
@@ -4,29 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Generic message payload used by SenderService
- * Works for EMAIL, WHATSAPP (and future channels)
- */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessagePayload {
 
-    /**
-     * EMAIL  -> email address
-     * WHATSAPP -> mobile number (with country code)
-     */
+    
     private String to;
 
-    /**
-     * Used only for EMAIL
-     * Ignored for WHATSAPP
-     */
+    
     private String subject;
 
-    /**
-     * Message body (required for all channels)
-     */
+    
     private String body;
+
 }
