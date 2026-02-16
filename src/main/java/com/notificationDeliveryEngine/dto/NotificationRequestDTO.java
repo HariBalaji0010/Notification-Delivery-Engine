14a79e455a79a72b9e1ec3047a735be55ec05466
Hari Balaji R <113185180+HariBalaji0010@users.noreply.github.com>
2026-02-16 11:54:14 +0530

    Update NotificationRequestDTO.java

--- src/main/java/com/notificationDeliveryEngine/dto/NotificationRequestDTO.java
+++ src/main/java/com/notificationDeliveryEngine/dto/NotificationRequestDTO.java
@@ -5,7 +5,8 @@ import lombok.Data;
 
 @Data
 public class NotificationRequestDTO {
-    private String recipient;       // email or mobile number
-    private ChannelType channel;    // EMAIL or WHATSAPP
-    private String message;         // notification content
+    private String recipient;       
+    private ChannelType channel;    
+    private String message;         
+
 }
